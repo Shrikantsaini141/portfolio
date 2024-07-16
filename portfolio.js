7e1389c2190c9a743386e27b477dc1e7438f1e9d
Shrikantsaini141 <shrikantsaini141@gmail.com>
2024-07-16 11:40:25 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,5 @@
+{/* <script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script> */}
+    var typed = new Typed('#element', {
+        strings: ['Web Developer', 'Web Designer', 'Game Developer', 'Video Editor'],
+        typeSpeed: 50,
+    });
